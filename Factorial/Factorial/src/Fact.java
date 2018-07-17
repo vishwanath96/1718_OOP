aaefd2002f2b526556a813116fae6100e8b628aa
Vishu <Vishu@MCALab-IV-20>
2018-07-17 15:18:50 +0530

    1st version of Factorial...

---
+++ Factorial/Factorial/src/Fact.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fact {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i,p=1,n;
+		System.out.print("Enter the Number: ");
+		n=scanner.nextInt();
+		scanner.nextLine();
+		for(i=1;i<=n;i++)
+		{
+			p=i*p;
+		}
+		System.out.println("Factorial= "+p);
+
+	}
+
+}
